fb360d3a789ee8f3fb1b0b03a1852b96e7629171
Francisco Rios <pacoriosgalan@gmail.com>
2021-04-25 19:01:30 +0200

    add index to 'price' field as it is used for filtering

--- models/Ad.js
+++ models/Ad.js
@@ -3,49 +3,52 @@
 const mongoose = require('mongoose');
 
 // Schema
-const adSchema = mongoose.Schema({
-  name: {
-  	type: String,
-  	trim: true,
-    required: true,
-    index: true
+const adSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      index: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    isType: {
+      type: String,
+      enum: ['vende', 'busca'],
+      required: true,
+      index: true,
+    },
+    price: {
+      type: Number,
+      index: true,
+    },
+    image: {
+      type: String,
+    },
+    tags: {
+      type: [String],
+      // enum: ["work", "lifestyle", "motor", "mobile"]
+      index: true,
+    },
   },
-  description: {
-  	type: String,
-  	trim: true,
+  {
+    collection: 'anuncios',
   },
-  isType: {
-    type: String,
-    enum: ["vende", "busca"],
-    required: true,
-    index: true
-	},
-  price: {
-    type: Number,
-	},
-  image: {
-    type: String,
-  },    
-  tags: {
-    type: [String],
-    // enum: ["work", "lifestyle", "motor", "mobile"]
-    index: true
-	}
-}, {
-  collection: 'anuncios'
-});
+);
 
-adSchema.statics.list = function(filter, limit, skip) {
+adSchema.statics.list = function (filter, limit, skip) {
   // Don't use arrow functions on Mongoose methods
   const query = Ad.find(filter);
   query.limit(limit);
   query.skip(skip);
   return query.exec();
-}
+};
 
 // Model
 const Ad = mongoose.model('Ad', adSchema);
 
 // Export model
 module.exports = Ad;
-
