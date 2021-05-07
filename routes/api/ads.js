d19cd966a0dfc3d246bedcd85743d36053a9507e
Francisco Rios <pacoriosgalan@gmail.com>
2021-05-07 08:09:17 +0200

    Library multer for uploading files

--- routes/api/ads.js
+++ routes/api/ads.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 const Ad = require('../../models/Ad');
 const asyncHandler = require('express-async-handler');
 const adsProvider = require('../../lib/adsProvider');
 const jwtAuth = require('../../lib/jwtAuth');
+var multer = require('multer');
+var multerConfig = multer({
+  dest: path.join(__dirname, './../../public/uploads'),
+  // Check extension file:
+  fileFilter: (req, file, next) => {
+    const filetypes = /jpeg|jpg|png|gif|webp|heic/;
+    const mimetype = filetypes.test(file.mimetype);
+    const fileExtension = filetypes.test(path.extname(file.originalname));
+    if (!mimetype || !fileExtension) {
+      const error = new Error('File selected is not a valid image');
+      error.status = 400;
+      next(error);
+      return;
+    }
+    return next(null, true);
+  },
+});
+// }).single('image'); //TODO:HANDLING MULTER ERROR, see /POST
 
 // GET /api/ads -> List ads
 router.get(
@@ -15,17 +34,50 @@ router.get(
   }),
 );
 
-// POST /api/ads (body)
+// POST / api / ads(body);
 router.post(
   '/',
+  multerConfig.single('image'),
   jwtAuth,
   asyncHandler(async (req, res) => {
-    const adData = req.body;
-    const ad = new Ad(adData);
+    const ad = new Ad(req.body);
+    ad.image =
+      req.file.filename +
+      path.extname(req.file.originalname).toLocaleLowerCase();
     const adCreated = await ad.save();
     res.status(201).json({ result: adCreated });
   }),
 );
+
+// POST /api/ads (body) //TODO:HANDLING MULTER ERROR
+// router.post(
+//   '/',
+//   jwtAuth,
+//   multerConfig(req, res, function (err) {
+//     if (err instanceof multer.MulterError) {
+//       // Multer error occurred when uploading
+//       const error = new Error('Error uploading file');
+//       error.status = 400;
+//       next(error);
+//       return;
+//     } else if (err) {
+//       // Unknown error occurred when uploading
+//       const error = new Error('Unknown error');
+//       error.status = 400;
+//       next(error);
+//       return;
+//     }
+//     // This way the image is not optional //TODO: Add this to ReadMe
+//     asyncHandler(async (req, res) => {
+//       const ad = new Ad(req.body);
+//       ad.image =
+//         req.file.filename +
+//         path.extname(req.file.originalname).toLocaleLowerCase();
+//       const adCreated = await ad.save();
+//       res.status(201).json({ result: adCreated });
+//     });
+//   }),
+// );
 
 // PUT /api/ads:id (body)
 router.put(
